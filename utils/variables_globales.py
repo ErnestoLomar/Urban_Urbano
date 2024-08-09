21eff271a7a756df6580956bd2fbf4d9706046f1
Ernesto Lomar <marioernestolomar@protonmail.ch>
2024-08-09 14:48:22 -0600

    EL.v2.47

--- utils/variables_globales.py
+++ utils/variables_globales.py
@@ -6,7 +6,7 @@
 # Script para almacenar las variables globales que se esten utilizando en el programa
 ##########################################
 
-version_del_software = "EL.v2.46"
+version_del_software = "EL.v2.47"
 banderaServicio=False
 longitud = 0
 latitud = 0
